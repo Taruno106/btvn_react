752b19bb215ddd3abc132cc27f8b34521be2ab9c
ToanPhung <phungmanhtoancd2001@gmail.com>
2022-01-22 22:28:29 +0700

    bt component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import BT_State_ChonKinh from './btvn/buoi4/BT_State_ChonKinh';
+import BaiTapThucHanhLayout from './btvn/BaiTapLayoutComponent/BaiTapThucHanhLayout';
 
 function App() {
   return (
     <div className="App">        
 
-      <BT_State_ChonKinh/>
+      {/* <BT_State_ChonKinh/> */}
+
+      <BaiTapThucHanhLayout />
     </div>
   );
 }
